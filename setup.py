a2d9dae179984a9f163921e41a0a9fb4ad85e26b
Adam Chainz <adam@adamj.eu>
2015-03-10 18:34:46 +0000

    The last release on pypi was screwed due to setup.py not including subpackages by default. Pinch a lot of boilerplate from django-rest-framework to solve this

--- setup.py
+++ setup.py
@@ -1,21 +1,62 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# -*- coding:utf-8 -*-
 from __future__ import print_function
 
 import os
+import re
+from setuptools import setup
 import sys
 
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
+def get_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
+
+
+version = get_version('django_mysql')
+
+
 if sys.argv[-1] == 'publish':
-    os.system("python setup.py sdist upload")
-    os.system("python setup.py bdist_wheel upload")
+    if os.system("pip freeze | grep wheel"):
+        print("wheel not installed.\nUse `pip install wheel`.\nExiting.")
+        sys.exit()
+    if os.system("pip freeze | grep twine"):
+        print("twine not installed.\nUse `pip install twine`.\nExiting.")
+        sys.exit()
+    os.system("python setup.py sdist bdist_wheel")
+    os.system("twine upload dist/*")
+    print("You probably want to also tag the version now:")
+    print("  git tag -a %s -m 'version %s'" % (version, version))
+    print("  git push --tags")
     sys.exit()
-
-
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
 
 
 with open('README.rst') as readme_file:
@@ -33,16 +74,13 @@ test_requirements = [
 
 setup(
     name='django-mysql',
-    version='0.1.3',
+    version=version,
     description="Extensions to Django for use with MySQL/MariaDB",
     long_description=readme + '\n\n' + history,
     author="Adam Johnson",
     author_email='me@adamj.eu',
     url='https://github.com/adamchainz/django-mysql',
-    packages=[
-        'django_mysql',
-    ],
-    package_dir={'django_mysql': 'django_mysql'},
+    packages=get_packages('django_mysql'),
     include_package_data=True,
     install_requires=requirements,
     license="BSD",
