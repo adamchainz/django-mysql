6cd85eee8a8a6d9034ac98ea27099d4a0e44828e
Adam Chainz <adam@adamj.eu>
2015-03-11 14:15:55 +0000

    Remove unneeded function from setup.py, remove setenv to use packaged files on tox

--- setup.py
+++ setup.py
@@ -25,22 +25,6 @@ def get_packages(package):
             if os.path.exists(os.path.join(dirpath, '__init__.py'))]
 
 
-def get_package_data(package):
-    """
-    Return all files under the root package, that are not in a
-    package themselves.
-    """
-    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
-            for dirpath, dirnames, filenames in os.walk(package)
-            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
-
-    filepaths = []
-    for base, filenames in walk:
-        filepaths.extend([os.path.join(base, filename)
-                          for filename in filenames])
-    return {package: filepaths}
-
-
 version = get_version('django_mysql')
 
 
