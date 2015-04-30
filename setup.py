772503c982c1d5d065c46faced07f427e4a7975a
Adam Chainz <adam@adamj.eu>
2015-04-30 10:45:38 +0100

    Stable not beta

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup(
     zip_safe=False,
     keywords='Django, MySQL, MariaDB',
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
