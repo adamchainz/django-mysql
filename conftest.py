bb88b1d2e2c4d3eb482c3cf32d1a53c9e89f94cf
Adam Chainz <adam@adamj.eu>
2015-10-22 13:24:55 +0100

    Fix pytest version report when database does not exist, add Django version header

--- conftest.py
+++ conftest.py
@@ -1,11 +1,18 @@
 # -*- coding:utf-8 -*-
 from __future__ import unicode_literals
 
+import django
 from django.db import connection
 
 
 def pytest_report_header(config):
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT VERSION()")
-        version = cursor.fetchone()[0]
-    return "MySQL version: {}".format(version)
+    dot_version = '.'.join(str(x) for x in django.VERSION)
+    header = "Django version: " + dot_version
+
+    if hasattr(connection, '_nodb_connection'):
+        with connection._nodb_connection.cursor() as cursor:
+            cursor.execute("SELECT VERSION()")
+            version = cursor.fetchone()[0]
+        header += "\nMySQL version: {}".format(version)
+
+    return header
