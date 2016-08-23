dd2a906a3459450aa58d5ba5aed8c5cc3d788f74
Adam Chainz <adam@adamj.eu>
2016-08-23 08:14:42 +0100

    Upgrade pytest and pytest-django

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,13 +3,14 @@ from __future__ import unicode_literals
 
 import django
 from django.db import connection
+from pytest_django.plugin import _blocking_manager
 
 
 def pytest_report_header(config):
     dot_version = '.'.join(str(x) for x in django.VERSION)
     header = "Django version: " + dot_version
 
-    if hasattr(connection, '_nodb_connection'):
+    with _blocking_manager.unblock():
         with connection._nodb_connection.cursor() as cursor:
             cursor.execute("SELECT VERSION()")
             version = cursor.fetchone()[0]
