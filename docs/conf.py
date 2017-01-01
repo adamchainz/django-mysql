7f3397faaf58aa7c4c21162fe298852b13d9f6fe
Adam Chainz <me@adamj.eu>
2017-01-01 19:32:29 +0000

    2017

--- docs/conf.py
+++ docs/conf.py
@@ -63,7 +63,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Django-MySQL'
-copyright = u'2016, Adam Johnson'
+copyright = u'2017, Adam Johnson'
 
 # The version info for the project you're documenting, acts as replacement
 # for |version| and |release|, also used in various other places throughout
