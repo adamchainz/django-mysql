b45d2f7a638d07260ddecedec83e252b30647907
Adam Johnson <me@adamj.eu>
2017-09-24 12:37:31 +0100

    Travis - test on Trusty (#388)
    As per [their blog post](https://blog.travis-ci.com/2017-07-11-trusty-as-default-linux-is-coming) they're making it the new default.

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -1,4 +1,7 @@
-#!/bin/bash -e
+#!/bin/bash
+
+set -e
+set -x
 
 # percona-toolkit - have to use non-apt version since Travis' ubuntu 12.04 repo
 # is way out of date
@@ -7,38 +10,38 @@ sudo apt-get install -y libio-socket-ssl-perl
 wget https://www.percona.com/downloads/percona-toolkit/2.2.13/deb/percona-toolkit_2.2.13_all.deb
 sudo dpkg -i percona-toolkit_2.2.13_all.deb
 
-# MySQL
+# DB
+
+# Nuke default
+sudo rm -rf /var/lib/mysql
 
 if [[ $DB == 'mysql' ]]
 then
-  if [[ $DB_VERSION == '5.5' ]]
-  then
-    # Travis default
-    sudo service mysql restart
-  else
-    # Nuke default
-    sudo apt-get -y purge mysql-server
-    sudo apt-get -y autoremove --purge
-    sudo rm -rf /var/lib/mysql
     # Install new
-    echo "deb http://repo.mysql.com/apt/ubuntu/ precise mysql-$DB_VERSION" | sudo tee /etc/apt/sources.list.d/mysql.list >/dev/null
-    echo "deb-src http://repo.mysql.com/apt/ubuntu/ precise mysql-$DB_VERSION" | sudo tee -a /etc/apt/sources.list.d/mysql.list >/dev/null
+    sudo add-apt-repository "deb http://repo.mysql.com/apt/ubuntu/ trusty mysql-$DB_VERSION"
+    sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 5072E1F5
     sudo apt-get update
-    yes Y | sudo DEBIAN_FRONTEND=noninteractive apt-get -y --force-yes install mysql-server
-  fi
-
+    echo 'Package: *
+Pin: origin repo.mysql.com
+Pin-Priority: 10000' | sudo tee /etc/apt/preferences.d/pin-mysql.pref
+    sudo DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-server
 elif [[ $DB == 'mariadb' ]]
 then
-  # Nuke default
-  sudo apt-get -y purge mysql-server
-  sudo apt-get -y autoremove --purge
-  sudo rm -rf /var/lib/mysql /etc/mysql
-  # Install
-  sudo apt-get install -y python-software-properties
-  sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
-  sudo add-apt-repository "deb http://ftp.osuosl.org/pub/mariadb/repo/$DB_VERSION/ubuntu precise main"
-  sudo apt-get update -qq
-  yes Y | sudo DEBIAN_FRONTEND=noninteractive apt-get install -y mariadb-server libmariadbclient-dev
+    # Install
+    sudo apt-get install -y software-properties-common
+    sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
+    sudo add-apt-repository "deb [arch=amd64,i386,ppc64el] http://sfo1.mirrors.digitalocean.com/mariadb/repo/$DB_VERSION/ubuntu trusty main"
+    echo 'Package: *
+Pin: origin sfo1.mirrors.digitalocean.com
+Pin-Priority: 10000' | sudo tee /etc/apt/preferences.d/pin-mariadb.pref
+    sudo apt-get update
+    PACKAGES="mariadb-server mariadb-client"
+    if [[ $DB_VERSION != '10.2' ]]
+    then
+        PACKAGES="$PACKAGES libmariadbclient-dev"
+    fi
+    # shellcheck disable=SC2086
+    sudo DEBIAN_FRONTEND=noninteractive apt-get install -y $PACKAGES
 fi
 
 sudo mysql -u root -e "create user travis@localhost identified by '';" || true
