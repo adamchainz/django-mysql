1221a98842ce1bdef5331b9e304aa85fa7a2c502
Adam Johnson <me@adamj.eu>
2017-11-04 10:03:09 +0000

    fix alias_map condition

--- django_mysql/models/handler.py
+++ django_mysql/models/handler.py
@@ -232,7 +232,7 @@ class Handler(object):
             not query.group_by and
             not query.distinct and
             not query.order_by and
-            len(query.alias_map) == 0
+            len(query.alias_map) <= 1
         )
 
         if hasattr(query, 'having'):  # Django < 1.9
