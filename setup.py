0797bc25cac4410e392588682e512c543514eda6
Adam Johnson <me@adamj.eu>
2018-02-16 10:55:43 -0500

    Add python_requires to setup.py

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
     install_requires=[
         'Django>=1.8',
     ],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     license="BSD",
     zip_safe=False,
     keywords=['Django', 'MySQL', 'MariaDB'],
