5fff45c9abeceaee72bf8d3b31cdac0631b5b104
Adam Johnson <me@adamj.eu>
2019-04-13 14:36:51 +0200

    Add Python 3 Only Classifier

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
