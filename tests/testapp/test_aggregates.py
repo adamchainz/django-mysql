5304b33b3a251fc32985d0bf5a511a5da809fddb
Javier Palacios <javier.palacios@wegow.com>
2020-04-26 16:45:50 +0200

    Fix distinct=True test

--- tests/testapp/test_aggregates.py
+++ tests/testapp/test_aggregates.py
@@ -111,7 +111,7 @@ class GroupConcatTests(TestCase):
         out = Author.objects.exclude(id=self.shakes.id).aggregate(
             tids=GroupConcat("tutor_id", distinct=True)
         )
-        assert out == {"tids": str(self.shakes.id)}
+        assert out == {"tids": set(str(self.shakes.id))}
 
     @override_mysql_variables(SQL_MODE="ANSI")
     def test_separator_ansi_mode(self):
