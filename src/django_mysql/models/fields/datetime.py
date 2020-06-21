6c8cfa316cb86f7ab0dfc6bd4ab4cef4b1f97a7f
KimSoungRyoul <KimSoungRyoul@gmail.com>
2020-06-21 20:33:58 +0900

    remove mysql DateTimeCheckMixin

--- src/django_mysql/models/fields/datetime.py
+++ src/django_mysql/models/fields/datetime.py
@@ -1,10 +1,43 @@
 from django.core import checks
 from django.db.models import DateTimeField as DjangoDateTimeField
-from django.db.models.fields import DateTimeCheckMixin as DjangoDateTimeCheckMixin
 from django.utils import timezone
 
 
-class DateTimeCheckMixin(DjangoDateTimeCheckMixin):
+class DateTimeField(DjangoDateTimeField):
+    def __init__(self, on_update_current_timestamp=False, **kwargs):
+        self.on_update_current_timestamp = on_update_current_timestamp
+        super(DateTimeField, self).__init__(**kwargs)
+
+    def db_type_suffix(self, connection):
+        db_type_suffix = super().db_type_suffix(connection)
+        if self.on_update_current_timestamp:
+            if db_type_suffix is None:
+                db_type_suffix = "ON UPDATE CURRENT_TIMESTAMP(6)"
+            else:
+                db_type_suffix += "ON UPDATE CURRENT_TIMESTAMP(6)"
+        return db_type_suffix
+
+    def pre_save(self, model_instance, add):
+        if self.auto_now or (self.default and add) or self.on_update_current_timestamp:
+            value = timezone.now()
+            setattr(model_instance, self.attname, value)
+            return value
+        else:
+            return getattr(model_instance, self.attname)
+
+    def deconstruct(self):
+        name, path, args, kwargs = super().deconstruct()
+        if self.on_update_current_timestamp:
+            kwargs["on_update_current_timestamp"] = True
+
+        bad_paths = (
+            "django_mysql.models.fields.datetime.DateTimeField",
+            "django_mysql.models.fields.DateTimeField",
+        )
+        if path in bad_paths:
+            path = "django_mysql.models.DateTimeField"
+        return name, path, args, kwargs
+
     def _check_mutually_exclusive_options(self):
         # auto_now, on_upd and default are mutually exclusive
         # options. The use of more than one of these options together
@@ -41,39 +74,3 @@ class DateTimeCheckMixin(DjangoDateTimeCheckMixin):
             ]
         else:
             return []
-
-
-class DateTimeField(DateTimeCheckMixin, DjangoDateTimeField):
-    def __init__(self, on_update_current_timestamp=False, **kwargs):
-        self.on_update_current_timestamp = on_update_current_timestamp
-        super(DateTimeField, self).__init__(**kwargs)
-
-    def db_type_suffix(self, connection):
-        db_type_suffix = super().db_type_suffix(connection)
-        if self.on_update_current_timestamp:
-            if db_type_suffix is None:
-                db_type_suffix = "ON UPDATE CURRENT_TIMESTAMP(6)"
-            else:
-                db_type_suffix += "ON UPDATE CURRENT_TIMESTAMP(6)"
-        return db_type_suffix
-
-    def pre_save(self, model_instance, add):
-        if self.auto_now or (self.default and add) or self.on_update_current_timestamp:
-            value = timezone.now()
-            setattr(model_instance, self.attname, value)
-            return value
-        else:
-            return getattr(model_instance, self.attname)
-
-    def deconstruct(self):
-        name, path, args, kwargs = super().deconstruct()
-        if self.on_update_current_timestamp:
-            kwargs["on_update_current_timestamp"] = True
-
-        bad_paths = (
-            "django_mysql.models.fields.datetime.DateTimeField",
-            "django_mysql.models.fields.DateTimeField",
-        )
-        if path in bad_paths:
-            path = "django_mysql.models.DateTimeField"
-        return name, path, args, kwargs
