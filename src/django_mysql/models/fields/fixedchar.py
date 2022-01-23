a7560e85049e0c70609fdfe25e2839448ab1f615
Adam Johnson <me@adamj.eu>
2022-01-23 09:45:33 +0000

    Remove redundant comments

--- src/django_mysql/models/fields/fixedchar.py
+++ src/django_mysql/models/fields/fixedchar.py
@@ -10,26 +10,22 @@ from django_mysql.typing import DeconstructResult
 
 class FixedCharField(CharField):
     def __init__(self, *args: Any, length: int, **kwargs: Any) -> None:
-        # Ensure we have an actual integer value
         if not isinstance(length, int):
             raise TypeError(
                 'Invalid length value "{length}". '
                 "Expected integer value.".format(length=length)
             )
 
-        # CHAR fields can only be in the range of 0-255
         if length < 0 or length > 255:
             raise ValueError(
                 'Invalid length value "{length}". '
                 "Length must be in the range of 0-255.".format(length=length)
             )
 
-        # A max_length doesn't make sense in this context
         if "max_length" in kwargs:
             raise TypeError('"max_length" is not a valid argument')
 
-        # However, a max_length is required by Django CharField,
-        # so we'll set it equal to the length of our CHAR field
+        # max_length is required by CharField
         self.length = length
         kwargs["max_length"] = length
         super().__init__(*args, **kwargs)
