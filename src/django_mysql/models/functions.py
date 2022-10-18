8758ed10b29cd8b9d56b1fa6f495062aa3c0bfd5
Adam Johnson <me@adamj.eu>
2022-10-18 19:32:29 +0100

    Improve function types

--- src/django_mysql/models/functions.py
+++ src/django_mysql/models/functions.py
@@ -169,7 +169,8 @@ class LastInsertId(Func):
         # database connections in Django, and the reason was not clear
         with connections[using].cursor() as cursor:
             cursor.execute("SELECT LAST_INSERT_ID()")
-            return cursor.fetchone()[0]
+            id_: int = cursor.fetchone()[0]
+            return id_
 
 
 # JSON Functions
@@ -258,7 +259,11 @@ class JSONValue(Expression):
         if connection.vendor != "mysql":  # pragma: no cover
             raise AssertionError("JSONValue only supports MySQL/MariaDB")
         json_string = json.dumps(self._data, allow_nan=False)
-        if connection.vendor == "mysql" and connection.mysql_is_mariadb:
+        if (
+            connection.vendor == "mysql"
+            # type narrowed by vendor check
+            and connection.mysql_is_mariadb  # type: ignore [attr-defined]
+        ):
             # MariaDB doesn't support explicit cast to JSON.
             return "JSON_EXTRACT(%s, '$')", (json_string,)
         else:
@@ -270,7 +275,7 @@ class BaseJSONModifyFunc(Func):
         self,
         expression: ExpressionArgument,
         data: dict[
-            str,
+            ExpressionArgument,
             (
                 ExpressionArgument
                 | None
@@ -288,12 +293,12 @@ class BaseJSONModifyFunc(Func):
         exprs = [expression]
 
         for path, value in data.items():
-            if not hasattr(path, "resolve_expression"):
+            if not isinstance(path, Expression):
                 path = Value(path)
 
             exprs.append(path)
 
-            if not hasattr(value, "resolve_expression"):
+            if not isinstance(value, Expression):
                 value = JSONValue(value)
 
             exprs.append(value)
@@ -392,7 +397,7 @@ class ColumnAdd(Func):
         self,
         expression: ExpressionArgument,
         to_add: dict[
-            str,
+            ExpressionArgument,
             ExpressionArgument | float | int | dt.date | dt.time | dt.datetime,
         ],
     ) -> None:
@@ -400,12 +405,12 @@ class ColumnAdd(Func):
 
         expressions = [expression]
         for name, value in to_add.items():
-            if not hasattr(name, "resolve_expression"):
+            if not isinstance(name, Expression):
                 name = Value(name)
 
-            if isinstance(value, dict):  # type: ignore [unreachable]
+            if isinstance(value, dict):
                 raise ValueError("ColumnAdd with nested values is not supported")
-            if not hasattr(value, "resolve_expression"):
+            if not isinstance(value, Expression):
                 value = Value(value)
 
             expressions.extend((name, value))
